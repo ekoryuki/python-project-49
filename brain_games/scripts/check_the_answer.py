d4cb443462fa025dadd3f066ee95b0be49d96f24
Elena K <happy.new.e.a@gmail.com>
2024-04-09 03:01:09 +0300

    update dist and brain games

--- brain_games/scripts/check_the_answer.py
+++ brain_games/scripts/check_the_answer.py
@@ -5,5 +5,5 @@ def check_the_answer(guess, corr, name):
             return +1
         case False:
             print(f"'{guess}' is wrong answer ;(. Correct answer was '{corr}'")
-            print(f"Let's try again, {name}")
+            print(f"Let's try again, {name}!")
             return -1
